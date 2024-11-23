f39f2f681f5760d6484999849bc35c03e68b994e
xietyBTW <kiryukha.siplyj@gmail.com>
2024-11-23 17:29:46 +0400

    Delete account.php

--- account.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'storage.php';
-$limit = isset($_GET['limit']) ? (int)$_GET['limit'] : 10;
-$limit = max(1, min($limit, 100)); 
-$users_list = array_slice($users, 0, $limit); 
-require 'account_page.html';
-?>
